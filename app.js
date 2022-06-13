9ce7012a47e469fd03a247000234e391540d213f
Umair Iqbal <umairazeem1999@gmail.com>
2022-06-13 10:21:25 +0500

    project done

---
+++ app.js
@@ -0,0 +1,9 @@
+var navLinks = document.getElementById("navLinks")
+// console.log(navLinks)
+
+function menuShow(){
+    navLinks.style.right = "0"
+}
+function menuHide(){
+    navLinks.style.right = "-200px"
+}
